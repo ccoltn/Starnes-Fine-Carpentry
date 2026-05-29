b426d5801190aabd964db27b6e1563c124f39a70
ccoltn <ccoltn@gmail.com>
2026-05-29 09:41:15 -0400

    Implement changeSlide function for slides and videos
    Add changeSlide function to manage slide transitions and video playback.

--- script.js
+++ script.js
@@ -26,3 +26,22 @@ function changeSlide(direction) {
 showSlide(currentSlide);
 
 
+function changeSlide(direction) {
+  slides[currentSlide].classList.remove("active");
+
+  currentSlide = (currentSlide + direction + slides.length) % slides.length;
+
+  slides[currentSlide].classList.add("active");
+
+  // Pause all videos
+  document.querySelectorAll("video").forEach(video => {
+    video.pause();
+    video.currentTime = 0;
+  });
+
+  // Play video in active slide
+  const activeVideo = slides[currentSlide].querySelector("video");
+  if (activeVideo) {
+    activeVideo.play();
+  }
+}
